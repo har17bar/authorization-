237ffcb63617a8b3046c9b3ef0767ae0ddf1ce50
Harut <har17bar@gmail.com>
2018-10-16 22:41:43 +0400

    Update users.js

--- users.js
+++ users.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const passport = require('passport');
-//Bring in article model
 const User = require('../models/user.js');
 
 router.get('/register', (req, res) => {
